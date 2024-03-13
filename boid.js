23a9664063b13c3208763ab29075df50582469b7
Mukund <mukund.srinivasan@wisetechglobal.com>
2024-03-13 13:59:49 +1100

    added sliders

--- boid.js
+++ boid.js
@@ -1,16 +1,24 @@
-const maxEnergy = 0.2;
-const maxSpeed = 5;
+// const maxEnergy = 0.2;
+// const maxSpeed = 5;
 
 const AlignConstant = 1.5;
 const CohesionConstant = 1;
 const SeparationConstant = 2;
 
 class Boid {
-    constructor() {
+    constructor(alignStrength, cohesionStrength, separationStrength, alignRadius, cohesionRadius, separationRadius, maxEnergy, maxSpeed) {
         this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
         this.acceleration = createVector();
         this.color = this.varyColour(color("orange"));
+        this.alignStrength = alignStrength;
+        this.cohesionStrength = cohesionStrength;
+        this.separationStrength = separationStrength;
+        this.alignRadius = alignRadius;
+        this.cohesionRadius = cohesionRadius;
+        this.separationRadius = separationRadius;
+        this.maxEnergy = maxEnergy;
+        this.maxSpeed = maxSpeed;
     }
 
     varyColour(baseColour) {
@@ -35,45 +43,59 @@ class Boid {
     }
 
     flock(boids) {
-        this.acceleration.add(this.align(boids).mult(AlignConstant));
-        this.acceleration.add(this.separate(boids).mult(SeparationConstant));
-        this.acceleration.add(this.cohere(boids).mult(CohesionConstant));
+        this.acceleration.add(this.align(boids).mult(this.alignStrength));
+        this.acceleration.add(this.separate(boids).mult(this.separationStrength));
+        this.acceleration.add(this.cohere(boids).mult(this.cohesionStrength));
+    }
+
+    cohere(boids) {
+        let cohesiveVector = createVector();
+        let neighbourCount = 0;
+        for (let boid of boids) {
+            if (p5.Vector.dist(this.position, boid.position) < this.cohesionRadius && this != boid) {
+                cohesiveVector.add(boid.position);
+                neighbourCount++;
+            }
+        }
+        if (neighbourCount > 0) {
+            cohesiveVector.div(neighbourCount);
+            cohesiveVector.sub(this.position);
+            cohesiveVector.setMag(this.maxSpeed);
+            cohesiveVector.sub(this.velocity);
+            cohesiveVector.limit(this.maxEnergy);
+        }
+        return cohesiveVector;
     }
 
     align(boids) {
-        let radius = 24;
         let alignmentVector = createVector();
         let neighbourCount = 0;
 
         for (let boid of boids) {
             let distance = p5.Vector.dist(boid.position, this.position);
-            if (boid != this && distance < radius) {
+            if (boid != this && distance < this.alignRadius) {
                 alignmentVector.add(boid.velocity);
                 neighbourCount++;
             }
         }
 
         if (neighbourCount > 0) {
-            //Find average of all velocities
             alignmentVector.div(neighbourCount);
-
-            // Set magnitude to max speed
-            alignmentVector.setMag(maxSpeed);
+            alignmentVector.setMag(this.maxSpeed);
             alignmentVector.sub(this.velocity);
-            alignmentVector.limit(maxEnergy);
+            alignmentVector.limit(this.maxEnergy);
         }
 
         return alignmentVector;
     }
 
     separate(boids) {
-        let radius = 25;
         let separationVector = createVector();
         let neighbourCount = 0;
 
         for (let boid of boids) {
             let displacement = p5.Vector.sub(this.position, boid.position);
-            if (boid != this && displacement.mag() < radius) {
+            if (boid != this && displacement.mag() < this.separationRadius) {
                 separationVector.add(displacement.div(displacement.mag() ** 2));
                 neighbourCount++;
             }
@@ -81,39 +103,18 @@ class Boid {
 
         if (neighbourCount > 0) {
             separationVector.div(neighbourCount);
-            separationVector.setMag(maxSpeed);
+            separationVector.setMag(this.maxSpeed);
             separationVector.sub(this.velocity);
-            separationVector.limit(maxEnergy);
+            separationVector.limit(this.maxEnergy);
         }
 
         return separationVector;
     }
 
-    cohere(boids) {
-        let cohereRadius = 100;
-        let cohesiveVector = createVector();
-        let neighbourCount = 0;
-        for (let boid of boids) {
-            if (p5.Vector.dist(this.position, boid.position) < cohereRadius && this != boid) {
-                cohesiveVector.add(boid.position);
-                neighbourCount++;
-            }
-        }
-        if (neighbourCount > 0) {
-
-            cohesiveVector.div(neighbourCount);
-            cohesiveVector.sub(this.position);
-            cohesiveVector.setMag(maxSpeed);
-            cohesiveVector.sub(this.velocity);
-            cohesiveVector.limit(maxEnergy);
-        }
-        return cohesiveVector;
-    }
-
     move() {
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
-        this.velocity.limit(maxSpeed);
+        this.velocity.limit(this.maxSpeed);
         this.acceleration.mult(0);
     }
 
--- sketch.js
+++ sketch.js
@@ -1,19 +1,93 @@
 let boids;
 
 function setup() {
-    var container = createCanvas(600, 600);
+    let container = createCanvas(600, 600);
     container.parent('container');
-    let slider = document.getElementById("boidCountSlider");
-    slider.oninput = function() {
-        doSetupActions(slider.value);
-    }
-    doSetupActions(50);
+
+    
+
+    let cohereStrSlider = document.getElementById("cohereStrength");
+    cohereStrSlider.oninput = function() {updateCohStrength(cohereStrSlider.value);}
+
+    let cohereRadSlider = document.getElementById("cohereRadius");
+    cohereRadSlider.oninput = function() {updateCohRad(cohereRadSlider.value);}
+
+    let alignStrSlider = document.getElementById("alignStrength");
+    alignStrSlider.oninput = function() {updateAliStrength(alignStrSlider.value);}
+
+    let alignRadSlider = document.getElementById("alignRadius");
+    alignRadSlider.oninput = function() {updateAliRad(alignRadSlider.value);}
+
+    let separateStrSlider = document.getElementById("separateStrength");
+    separateStrSlider.oninput = function() {updateSepStrength(separateStrSlider.value);}
+
+    let separateRadSlider = document.getElementById("separateRadius");
+    separateRadSlider.oninput = function() {updateSepRad(separateRadSlider.value);}
+
+    let speedSlider = document.getElementById("speedSlider");
+    speedSlider.oninput = function() {updateV(speedSlider.value);}
+
+    let energySlider = document.getElementById("energySlider");
+    energySlider.oninput = function() {updateE(energySlider.value);}
+
+    let boidCountSlider = document.getElementById("boidCountSlider");
+    boidCountSlider.oninput = function() {doSetupActions(boidCountSlider.value, cohereStrSlider.value, alignStrSlider.value, separateStrSlider.value, cohereRadSlider.value, alignRadSlider.value, separateRadSlider.value, 5, 0.2);}
+
+    doSetupActions(50, 1, 1.5, 2, 50, 25, 25, 5, 0.2);
 }
 
-function doSetupActions(boidCount) {
+function doSetupActions(boidCount, cohereStr, alignStr, sepStr, cohereRad, alignRad, sepRad, maxV, maxE) {
     boids = [];
     for (let i = 0; i < boidCount; i++) {
-        boids.push(new Boid());
+        boids.push(new Boid(alignStr, cohereStr, sepStr, alignRad, cohereRad, sepRad, maxE, maxV));
+    }
+}
+
+function updateCohRad(newCohRad) {
+    for (let boid of boids) {
+        boid.cohesionRadius = newCohRad;
+    }
+}
+
+function updateAliRad(newAliRad) {
+    for (let boid of boids) {
+        boid.alignRadius = newAliRad;
+    }
+}
+
+function updateSepRad(newSepRad) {
+    for (let boid of boids) {
+        boid.separationRadius = newSepRad;
+    }
+}
+
+function updateSepStrength(newStrength) {
+    for (let boid of boids) {
+        boid.separationStrength = parseInt(newStrength);
+    }
+}
+
+function updateCohStrength(newStrength) {
+    for (let boid of boids) {
+        boid.cohesionStrength = parseInt(newStrength);
+    }
+}
+
+function updateAliStrength(newStrength) {
+    for (let boid of boids) {
+        boid.alignStrength = parseInt(newStrength);
+    }
+}
+
+function updateV(newV) {
+    for (let boid of boids) {
+        boid.maxSpeed = parseInt(newV);
+    }
+}
+
+function updateE(newE) {
+    for (let boid of boids) {
+        boid.maxEnergy = parseInt(newE);
     }
 }
 
